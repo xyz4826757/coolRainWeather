e33bc704c2612730093155c4085c427313dafa33
xyz <762362967@qq.com>
2017-03-18 20:53:17 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolrainweather/android/db/City.java
@@ -0,0 +1,46 @@
+package com.coolrainweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xyz on 2017/3/18.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
